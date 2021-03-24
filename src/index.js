e14e0dd4b0d80117871b90be1986b73254857220
esellers25 <erin.e.sellers@gmail.com>
2021-03-24 15:40:23 -0400

    lecture updates

--- src/index.js
+++ src/index.js
@@ -28,16 +28,35 @@ document.addEventListener("DOMContentLoaded", function(){
 function makeToy(toy){
     let toyCard = document.createElement("div")
     toyCard.className = "card"
+    toyCard.innerText = toy.name
     let toyName = document.createElement("h2")
     let toyImage = document.createElement("img")
     toyImage.src = `${toy.image}`
     toyImage.className = "toy-avatar"
     let toyLikes = document.createElement("p")
+    toyLikes.innerText = `${toy.likes} Likes`
     let toyButton = document.createElement("button")
     toyButton.className = "like-btn"
+    toyButton.innerText = "Like"
     toyCard.append(toyName, toyImage, toyLikes, toyButton)
     let toyCollection = document.querySelector("#toy-collection")
     toyCollection.append(toyCard)
+
+    toyButton.addEventListener("click", function(){
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          likes: toy.likes + 1 
+        })
+      }).then(res => res.json())
+        .then(function(updatedLike){
+          toy.likes = updatedLike.likes
+          toyLikes.innerText = `${updatedLike.likes} Likes`
+        })
+    })
   }
 
 let toyForm = document.querySelector("form.add-toy-form")
